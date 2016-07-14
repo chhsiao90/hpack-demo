a3162b97bc35c8758f505bd8339c4d7f605f52a1
chhsiao90 <chhsiao90@gmail.com>
2016-07-14 13:43:45 +0800

    Translate each byte for every hpack package

--- demo.py
+++ demo.py
@@ -2,12 +2,16 @@ import binascii
 import sys
 from hpack import Encoder, Decoder
 import json
+import struct
 
 
 def header_dict_to_tuple(header):
     assert len(header) == 1
     item = header.items()[0]
-    return (item[0], item[1])
+    if item[0] in (b"cookie", u"cookie"):
+        return (item[0], item[1], True)
+    else:
+        return (item[0], item[1])
 
 
 def translate_match(match):
@@ -18,6 +22,67 @@ def translate_match(match):
     if not value:
         return "(patial match with index: {0})".format(index)
     return "(perfect match with index: {0})".format(index)
+
+
+def translate_byte(one_byte_data, match, curr_state, record_len=0):
+    if not curr_state:
+        if one_byte_data[0] == "1":
+            if "0" not in one_byte_data[1:]:
+                return ("int_end", "Indexed: ...", 127)
+            else:
+                return ("end", "Indexed: {0}".format(int(one_byte_data[1:], base=2)), 0)
+        elif one_byte_data[0:2] == "01":
+            if "1" not in one_byte_data[2:]:
+                return ("key_len", "Incremental Indexing -- New Name", 0)
+            elif "0" not in one_byte_data[2:]:
+                return ("int_value_len", "Incremental Indexing -- Indexed: ...", 63)
+            else:
+                return ("value_len", "Incremental Indexing -- Indexed: {0}".format(int(one_byte_data[2:], base=2)), 0)
+        elif one_byte_data[0:4] == "0000":
+            if "1" not in one_byte_data[4:]:
+                return ("key_len", "without Indexing -- New Name", 0)
+            elif "0" not in one_byte_data[4:]:
+                return ("int_value_len", "without Indexing -- Indexed: ...", 15)
+            else:
+                return ("value_len", "without indexing -- indexed: {0}".format(int(one_byte_data[4:], base=2)))
+        elif one_byte_data[0:4] == "0001":
+            if "1" not in one_byte_data[4:]:
+                return ("key_len", "never Indexing -- New Name", 0)
+            elif "0" not in one_byte_data[4:]:
+                return ("int_value_len", "never Indexing -- Indexed: ...", 15)
+            else:
+                return ("value_len", "never indexing -- indexed: {0}".format(int(one_byte_data[4:], base=2)))
+        else:
+            raise Exception(one_byte_data)
+
+    if curr_state.startswith("int"):
+        if "0" not in one_byte_data:
+            record_len += 255
+            return (curr_state, "int: 255", record_len)
+        record_len += int(one_byte_data, base=2)
+        return (curr_state[4:], "int sum: {0}".format(record_len), record_len)
+
+    if curr_state.startswith("str"):
+        record_len -= 1
+        if record_len:
+            return (curr_state, "", record_len)
+        return (curr_state[4:], "", record_len)
+
+    if curr_state == "key_len":
+        huff = "huffman" if one_byte_data[0] == "1" else "non-huffman"
+        if "0" not in one_byte_data[1:]:
+            return ("int_str_value_len", "{0} key, len: ...".format(huff), 127)
+        length = int(one_byte_data[1:], base=2)
+        return ("str_value_len", "{0} key, len: {1}".format(huff, length), length)
+
+    if curr_state == "value_len":
+        huff = "huffman" if one_byte_data[0] == "1" else "non-huffman"
+        if "0" not in one_byte_data[1:]:
+            return ("int_str_end", "{0} value, len: ...".format(huff), 127)
+        length = int(one_byte_data[1:], base=2)
+        return ("str_end", "{0} value, len: {1}".format(huff, length), length)
+
+    return ("state_failed", "state wrong: " + curr_state, 0)
 
 
 class Demo(object):
@@ -35,9 +100,20 @@ class Demo(object):
             encoded_len += len(encoded)
             origin_len += len(header_tuple[0]) + len(header_tuple[1])
             match = self.decoder.header_table.search(header_tuple[0], header_tuple[1])
-            print "{0}=>{1} {2}".format(header, binascii.hexlify(encoded), translate_match(match))
+
+            print "{0}=>{1}".format(header, binascii.hexlify(encoded), translate_match(match))
+            print translate_match(match)
+
+            curr_state = None
+            length = 0
+            for b in encoded:
+                one_byte_data = bin(struct.unpack("B", b)[0])[2:].zfill(8)
+                curr_state, content, length = translate_byte(one_byte_data, match, curr_state, length)
+                if content:
+                    print "{0} ({1})".format(one_byte_data, content)
 
             self.decoder.decode(encoded)
+            print
         print "Decompressed from {0} to {1}".format(origin_len, encoded_len)
 
     def pretty_print_table(self, table):
@@ -45,10 +121,7 @@ class Demo(object):
             print "{0}=>{1}".format(k, v)
 
     def tables(self):
-        print "[encoder]"
         self.pretty_print_table(self.encoder.header_table)
-        print "[decoder]"
-        self.pretty_print_table(self.decoder.header_table)
 
 
 class StoryRunner(object):
